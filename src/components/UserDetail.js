4065981c4ba9c83d3ab75f1a51a858b0a5bd6bd9
ciromuanya <c.iromuanya@gmail.com>
2025-06-23 13:44:07 -0400

    Add UserDetail component to display user information and purchase history

---
+++ src/components/UserDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import UserEditForm from "./UserEditForm";
+import SubscriptionEditor from "./SubscriptionEditor";
+
+function UserDetail({ user }) {
+  return (
+    <div style={{ marginTop: '1rem' }}>
+      <UserEditForm user={user} />
+      <SubscriptionEditor user={user} />
+      <div style={{ marginTop: '1rem' }}>
+        <h3>Purchase History</h3>
+        <ul>
+          {user.purchaseHistory.map((p) => (
+            <li key={p.id}>
+              {p.date}: {p.item} - ${p.amount.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default UserDetail;
