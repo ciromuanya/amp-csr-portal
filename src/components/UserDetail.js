4657090e9391051a157d8ec615774b9a0af74006
ciromuanya <c.iromuanya@gmail.com>
2025-06-26 16:28:07 -0400

    Add vehicle thumbnails section to UserDetail component

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,13 +1,26 @@
 import React from "react";
 import UserEditForm from "./UserEditForm";
 import SubscriptionEditor from "./SubscriptionEditor";
+import VehicleThumbnail from "./VehicleThumbnail";
 
 function UserDetail({ user, setUsers }) {
   return (
     <div style={{ marginTop: '1rem' }}>
       <UserEditForm user={user} setUsers={setUsers} />
-      <SubscriptionEditor user={user} setUsers={setUsers}/>
-      <div style={{ marginTop: '1rem' }}>
+      <SubscriptionEditor user={user} setUsers={setUsers} />
+
+      {/* Vehicle Thumbnails */}
+      <div style={{ marginTop: '2rem' }}>
+        <h3>Vehicles</h3>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {user.vehicles.map((vehicle) => (
+            <VehicleThumbnail key={vehicle.id} vehicle={vehicle} />
+          ))}
+        </div>
+      </div>
+
+      {/* Purchase History */}
+      <div style={{ marginTop: '2rem' }}>
         <h3>Purchase History</h3>
         <ul>
           {user.purchaseHistory.map((p) => (
